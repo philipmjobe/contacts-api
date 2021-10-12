e1ff3b2640333c5e7f17f185fedfbacb1f28149b
Philip <philipmjobe@gmail.com>
2021-10-12 15:14:26 -0400

    added more contacts to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Contact.create(first_name: "Brian", last_name: "Jobe", email: "brian@gmail.com", phone_number: "883939339")
+Contact.create(first_name: "Philip", last_name: "Jobe", email: "philip@gmail.com", phone_number: "73849738")
+Contact.create(first_name: "Mary", last_name: "McMillen", email: "mary@gmail.com", phone_number: "8848891489")
+Contact.create(first_name: "Jennifer", last_name: "Sturgill", email: "jenn@gmail.com", phone_number: "84952898")
